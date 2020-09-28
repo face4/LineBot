01b79e68359225d79ed00e7f7f771322f46e93fd
face4 <s1611361@u.tsukuba.ac.jp>
2020-09-28 11:58:12 +0900

    init

---
+++ src/main/java/LineBotApplication.java
@@ -0,0 +1,27 @@
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@LineMessageHandler
+public class LineBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LineBotApplication.class, args);
+    }
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage("「" + event.getMessage().getText() + "！」");
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}
