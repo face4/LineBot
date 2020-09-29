1a61aac3a37f8b7794a2d2b922518414d3fc8dfe
face4 <s1611361@u.tsukuba.ac.jp>
2020-09-29 10:32:34 +0900

    fix

--- src/main/java/com/app/LineBotApplication.java
+++ src/main/java/com/app/LineBotApplication.java
@@ -20,7 +20,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-@Controller
 @SpringBootApplication
 @LineMessageHandler
 public class LineBotApplication {
